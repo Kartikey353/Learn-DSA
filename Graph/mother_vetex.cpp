1e88f0bf2a6cd3038109cb371803c1afd169a60e
asv02 <96387070+asv02@users.noreply.github.com>
2022-10-04 00:42:37 +0530

    Update mother_vetex.cpp

--- Graph/mother_vetex.cpp
+++ Graph/mother_vetex.cpp
@@ -1,4 +1,4 @@
-/*https://practice.geeksforgeeks.org/problems/mother-vertex/1*/
+/*https://practice.geeksforgeeks.org/problems/mother-vertex/1 */
 
 /*Given a Directed Graph, find a Mother Vertex in the Graph (if present). 
 A Mother Vertex is a vertex through which we can reach all the other vertices of the Graph.*/
